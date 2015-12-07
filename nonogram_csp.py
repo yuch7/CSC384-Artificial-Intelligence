5ce9a310c1fc1ba0d024aee55e9d4f4863e280f0
yuch7 <yuchen7990@gmail.com>
2015-12-06 20:53:48 -0500

    works

--- nonogram_csp.py
+++ nonogram_csp.py
@@ -2,7 +2,7 @@ from cspbase import *
 import sys
 
 
-def nonogram_csp_model(c):
+def nonogram_csp_model(initial_board):
     """
     Return a CSP object representing a nonogram CSP problem along with
     an array of Variables for the problem. That is, return
@@ -10,13 +10,11 @@ def nonogram_csp_model(c):
     nonogram_csp, variable_array
     """
 
-    print(c[0])
-    print(c[1])
     lcsp = []
     board = []
-    for i in range(len(c[0])):
+    for i in range(len(initial_board[0])):
         board.append([])
-        for j in range(len(c[1])):
+        for j in range(len(initial_board[1])):
             board[i].append([])
             newvar = Variable("{}{}".format(i, j), [0, 1])
             board[i][j] = newvar
@@ -24,25 +22,32 @@ def nonogram_csp_model(c):
 
     nonogram_csp = CSP("Nonogram", lcsp)
 
-    for i in range(len(c[0])):
-        for j in range(i):
+    for i in range(len(initial_board[0])):
+        for j in range(len(initial_board[0][i])):
             sat_tuples = []
-            l = sum(c[0][i][:j]) + len(c[0][i][:j])
-            r = sum(c[0][i][j + 1:]) + len(c[0][i][j + 1:])
-            var = board[i][l:-r]
-            sat_tuples = get_sat_tuples(len(var), c[0][i][j])
-            c = Constraint("row{} consecutive: {}".format(i, c[0][i][j]), var)
+            l = sum(initial_board[0][i][:j]) + len(initial_board[0][i][:j])
+            r = sum(initial_board[0][i][j+1:]) + len(initial_board[0][i][j+1:])
+            if r == 0:
+                var = board[i][l:]
+            else:
+                var = board[i][l:-r]
+            sat_tuples = get_sat_tuples(len(var), initial_board[0][i][j])
+            c = Constraint("row{} consecutive: {}".format(i, initial_board[0][i][j]), var)
             c.add_satisfying_tuples(sat_tuples)
             nonogram_csp.add_constraint(c)
 
-    for i in range(len(c[1])):
-        for j in range(i):
+    for i in range(len(initial_board[1])):
+        for j in range(len(initial_board[1][i])):
             sat_tuples = []
-            l = sum(c[1][i][:j]) + len(c[1][i][:j])
-            r = sum(c[1][i][j + 1:]) + len(c[1][i][j + 1:])
-            var = [w[j] for w in board[l:-r]]
-            sat_tuples = get_sat_tuples(len(var), c[1][i][j])
-            c = Constraint("col{} consecutive: {}".format(i, c[1][i][j]), var)
+            l = sum(initial_board[1][i][:j]) + len(initial_board[1][i][:j])
+            r = sum(initial_board[1][i][j+1:]) + len(initial_board[1][i][j+1:])
+            print(i)
+            if r == 0:
+                var = [ w[i] for w in board[l:] ]
+            else:    
+                var = [ w[i] for w in board[l:-r] ]
+            sat_tuples = get_sat_tuples(len(var), initial_board[1][i][j])
+            c = Constraint("col{} consecutive: {}".format(i, initial_board[1][i][j]), var)
             c.add_satisfying_tuples(sat_tuples)
             nonogram_csp.add_constraint(c)
 
@@ -79,7 +84,7 @@ def nonogram_parse(filename):
 def get_sat_tuples(space, num):
     sat = []
     if (space == num):
-        return [1 for x in range(num)]
+        return [[1 for x in range(num)]]
     if (space < num):
         print("Error! Invalid file.")
         sys.exit()
