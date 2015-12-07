049f92d68f825dc6ed3d8e54dfe622b3f3571995
Abrar Hussain <abrar.a.hussain@gmail.com>
2015-12-06 19:42:42 -0500

    allow command line enter

--- nonogram_csp.py
+++ nonogram_csp.py
@@ -1,40 +1,54 @@
 from cspbase import *
 import sys
 
-def nonogram_csp(c):
-	"""
+
+def nonogram_csp_model(initial_board):
+    """
     Return a CSP object representing a nonogram CSP problem along with
     an array of Variables for the problem. That is, return
 
     nonogram_csp, variable_array
     """
 
-	board = []
-	for i in range(len(c[0])):
-		board.append([])
-		for j in range(len(c[1])):
-			board.append([])
-			board[i][j] = Variable("{}{}".format(i, j), [0,1])
+    nonogram_csp, variable_array = construct_base_csp(initial_board)
 
- 	lcsp = []
+    for row in varialbe_array:
+        pass
+
+    transpose = list(map(list, zip(*variable_array)))
+    for row in transpose:
+        pass
+
+    return (nonogram_csp, variable_array)
 
 
 def nonogram_parse(filename):
-	H = []
-	V = []
+    """
+    Take a specific filename as input, and then parse the
+    file in order to produce an initial nonogram form. Assumes
+    that the file is a valid nonogram file.
 
-	tosolve = open(filename, "r")
-	line = tosolve.readline()
-	print(line)
-	if (line != 'H'):
-		print("Invalid file!")
-		sys.exit()
-	while (line != "V"):
-		line = tosolve.readline()
-		H.append(line.split())
-	while (line != ''):
-		line = tosolve.readline()
-		V.append(line.split())
+    Returns a tuple of the form (list, list).
+    """
+    rows = []
+    columns = []
 
-	tosolve.close()
-	return (H, V)
+    with open(filename) as f:
+
+        for line in f:
+            rows.append(line.split())
+            if line == "V":
+                break
+        for line in f:
+            columns.append(line.split())
+
+    return (rows, columns)
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Usage: python3 nonogram_csp <filename>")
+        sys.exit()
+
+    nonogram_name = sys.argv[1]
+    nonogram_parse(nonogram_name)
+    # TODO: solve
